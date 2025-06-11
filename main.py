2f30a8b7290efd77fcf9271f712478b936fda3c7
Pedro Calderón <pedrocalderonn52@gmail.com>
2025-06-11 17:36:41 -0300

    arquivo com algoritmo pronto, sem interface e sem correção no tamanho das imagens

---
+++ main.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import pyperclip as ppc
+
+gray_scale = ["@@", "%%", "##", "**", "++", "==", "--", "::", "..", "  "]
+
+
+img = Image.open("image.png")
+img_cinza = img.convert("L")
+img_cinza = img_cinza.resize((600, 600))
+
+img_cinza.save("cr7cinza.png")
+
+pixels = img_cinza.load()
+txt = ""
+print(img_cinza.size)
+for y in range(img_cinza.height):
+    for x in range(img_cinza.width):
+        txt += gray_scale[min(int(pixels[x, y] / 256 * len(gray_scale)), len(gray_scale) - 1)]
+    txt += "\n"
+    
+img_cinza.show()
+ppc.copy(txt)
+print("Arte Ascii copiada para sua área de transferência!")
+
